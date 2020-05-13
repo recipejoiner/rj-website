2dd6b53349f55d3beb2a95f330a9bb96d25aa394
Ari Mendelow <ari@mendelow.me>
2020-05-12 18:48:11 -0700

    Add query for getting all recipes

---
+++ requests/recipes.tsx
@@ -0,0 +1,45 @@
+import gql from 'graphql-tag';
+
+interface ShortRecipeInfo {
+  cursor: string;
+  node: {
+    id: string;
+    by: {
+      username: string;
+    }
+    title: string;
+    handle: string;
+    description: string;
+    servings: string;
+  }
+}
+export interface AllRecipesData {
+  allRecipes: {
+    pageInfo: {
+      hasNextPage: boolean;
+    }
+    edges: Array<ShortRecipeInfo>;
+  }
+}
+export const ALL_RECIPES = gql`
+  query getAllRecipes($cursor: String) {
+    allRecipes(first: 21, after: $cursor) {
+      pageInfo {
+        hasNextPage
+      }
+      edges {
+        cursor
+        node {
+          id
+          by {
+            username
+          }
+          title
+          handle
+          description
+          servings
+        }
+      }
+    }
+  }
+`;

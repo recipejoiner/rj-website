50f4c2e6047d48298d5d59ae97e093929a55b271
Ari Mendelow <ari@mendelow.me>
2020-05-13 11:48:04 -0700

    Factor our infinite scroll. This will allow any page that requires infinite scroll pagination to use this component. I was able to do this by: - Generalizing types for paginated pages, and modifying the query to reflect that - Creating a new functional component for infinite scrolling that takes in the query as well as the various types associated with it, and an object to initialize the query result with - Allowing 'children' to be passed in as a render prop that gets 'edges' as an argument. 'edges' is defined using the types passed in via props - This allows 'children', which is the portion rendered using the query result, to define what it wants to take in as well as how it wants to be rendered I also added the head title and meta tags

--- requests/recipes.tsx
+++ requests/recipes.tsx
@@ -1,37 +1,35 @@
 import gql from 'graphql-tag';
 
-interface ShortRecipeInfo {
-  cursor: string;
-  node: {
-    id: string;
-    by: {
-      username: string;
-      __typename: string;
-    }
-    title: string;
-    handle: string;
-    description: string;
-    servings: string;
+import { EdgeType } from 'components/InfiniteScroll';
+
+export interface ShortRecipeInfoType {
+  id: string;
+  by: {
+    username: string;
     __typename: string;
   }
+  title: string;
+  handle: string;
+  description: string;
+  servings: string;
   __typename: string;
 }
 export interface AllRecipesData {
-  allRecipes: {
+  result: {
     pageInfo: {
       hasNextPage: boolean;
       __typename: string;
     }
-    edges: Array<ShortRecipeInfo>;
+    edges: Array<EdgeType<ShortRecipeInfoType>>;
     __typename: string;
   }
 }
-export interface AllRecipesVars {
-  cursor: string;
+export interface AllRecipesVarsType {
+  cursor: string | null;
 }
 export const ALL_RECIPES = gql`
   query getAllRecipes($cursor: String) {
-    allRecipes(first: 10, after: $cursor) {
+    result: allRecipes(first: 10, after: $cursor) {
       pageInfo {
         hasNextPage
       }

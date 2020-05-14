25a14aeee45524623bb5369346aeebd195439d56
Ari Mendelow <ari@mendelow.me>
2020-05-13 19:34:45 -0700

    Add _document for setting the lang attribute of the page

---
+++ pages/_document.jsx
@@ -0,0 +1,52 @@
+// This page literally just exists so that we can set the language in the HTML tag and things like GTM
+// _document is only rendered on the server side and not on the client side
+// Event handlers like onClick can't be added to this file
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* put any head tags here, eg Google Tag Manager */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+// `getInitialProps` belongs to `_document` (instead of `_app`),
+// it's compatible with server-side generation (SSG).
+MyDocument.getInitialProps = async (ctx) => {
+  // Resolution order
+  //
+  // On the server:
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. document.getInitialProps
+  // 4. app.render
+  // 5. page.render
+  // 6. document.render
+  //
+  // On the server with error:
+  // 1. document.getInitialProps
+  // 2. app.render
+  // 3. page.render
+  // 4. document.render
+  //
+  // On the client
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. app.render
+  // 4. page.render
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+  };
+};

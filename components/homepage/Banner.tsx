a31f49560cd0cd4763dae35015a554776e726250
Ari Mendelow <ari@mendelow.me>
2020-05-14 18:40:22 -0700

    Update banner text

--- components/homepage/Banner.tsx
+++ components/homepage/Banner.tsx
@@ -5,13 +5,10 @@ function Banner() {
 			<div className="lg:flex m-auto">
 				<div className="hidden lg:block xl:hidden relative px-2 text-gray-900 w-1/2">
 					<div className="py-24 pl-10 pr-5">
-						<h1 className="font-semibold text-3xl lg:text-4xl leading-tight uppercase">Medical Equipment<br/>Made Easy</h1>
+						<h1 className="font-semibold text-3xl lg:text-4xl leading-tight uppercase">RecipeJoiner:<br/>A Chef's Home</h1>
 						<div className="mt-2 lg:mt-4 text-lg lg:text-2xl">
 							<div>
-								Offering quality medical equipment with in-house service at the best prices available.
-							</div>
-							<div className="lg:pt-4">
-								Call us today at <a id="callnumber" href="tel:+18452742204" className="font-semibold underline"> +1 (845) 274-2204‬</a> for a quote!
+								The perfect place to keep and share whatever's going on in your kitchen.
 							</div>
 						</div>
 					</div>
@@ -20,8 +17,8 @@ function Banner() {
 				<div className="relative pb-64 sm:pb-80 lg:pb-96 xl:pb-136 lg:w-1/2 xl:w-full">
 					<img
 						className="absolute h-full w-full object-cover"
-						src={require('images/misc/rj-banner-1').src}
-						srcSet={require('images/misc/rj-banner-1?resize&sizes[]=300&sizes[]=600&sizes[]=1000&sizes[]=1280&sizes[]=1920').srcSet}
+						src={require('images/misc/rj-banner-1.jpg').src}
+						srcSet={require('images/misc/rj-banner-1.jpg?resize&sizes[]=300&sizes[]=600&sizes[]=1000&sizes[]=1280&sizes[]=1920').srcSet}
 						alt="Medical professional performing an ultrasound" 
 					/>
 				</div>
@@ -30,13 +27,10 @@ function Banner() {
 			<div className="flex justify-center">
 				<div className="relative bg-white px-2 text-gray-900 lg:hidden xl:block xl:w-256 xl:-m-32 xl:mb-1 xl:shadow-xl xl:rounded-lg xl:py-2">
 					<div className="p-1 pt-6 pb-6">
-						<h1 className="font-semibold text-3xl lg:text-4xl leading-tight uppercase text-center">Medical Equipment<br className="xl:hidden"/> Made Easy</h1>
+						<h1 className="font-semibold text-3xl lg:text-4xl leading-tight uppercase text-center">RecipeJoiner:<br className="xl:hidden"/>A Chef's Home</h1>
 						<div className="mt-2 lg:mt-4 text-lg lg:text-2xl text-center">
 							<div>
-								Offering quality medical equipment with in-house service at the best prices available.
-							</div>
-							<div className="lg:pt-4">
-								Call us today at <a id="callnumber" href="tel:+18452742204" className="font-semibold underline"> +1 (845) 274-2204‬</a> for a quote!
+								The perfect place to keep and share whatever's going on in your kitchen.
 							</div>
 						</div>
 					</div>

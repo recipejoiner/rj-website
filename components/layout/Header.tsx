e35437b42523656b522ee27e00131bd66aa9aecb
Ari Mendelow <ari@mendelow.me>
2020-05-14 18:04:36 -0700

    Start filling out header

---
+++ components/layout/Header.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Link from 'next/link';
+
+interface HeaderProps {}
+
+const Header: React.FC<HeaderProps> = ({}) => {
+  return(
+    <header>
+      Header here!
+    </header>
+  );
+}
+
+export default Header;

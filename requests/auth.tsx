209513c0aa4432eb0d91d8b5b74915cba3695329
Ari Mendelow <ari@mendelow.me>
2020-05-21 22:35:00 -0700

    Remove unneeded return items from login mutation and add return type

--- requests/auth.tsx
+++ requests/auth.tsx
@@ -1,16 +1,21 @@
 import gql from 'graphql-tag';
 
+export interface UserLoginType {
+  login: {
+    user: {
+      token: String;
+    }
+  }
+}
+export interface LoginVarsType {
+  email: string;
+  password: string;
+}
 export const LOGIN = gql`
   mutation login($email: String, $password: String) {
     login(email: $email, password: $password) {
       user {
-        id
-        email
-        username
-        name
         token
-        createdAt
-        updatedAt
       }
     }
   }

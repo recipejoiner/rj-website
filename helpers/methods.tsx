5fae1956aee56e5e5b4f96816ad5f49795bde4ce
Ari Mendelow <ari@mendelow.me>
2020-05-23 11:52:58 -0700

    Add methods for dealing with cookies

--- helpers/methods.tsx
+++ helpers/methods.tsx
@@ -11,3 +11,33 @@ export function useEvent(event: string, handler: () => void, passive=false) {
     };
   });
 }
+
+// cookie utilities
+export function setCookie(name: string, val: string) {
+  const date = new Date();
+
+  // set expiration date to 4 weeks
+  date.setTime(date.getTime() + (4 * 7 * 24 * 60 * 60 * 1000));
+
+  // set the cookie
+  document.cookie = `${name}=${val};expires=${date.toUTCString()};path=/`;
+}
+
+export function getCookie(name: string) {
+  const val = `; ${document.cookie}`;
+  const parts = val.split(`; ${name}=`);
+
+  if (parts.length === 2) {
+    return parts.pop()?.split(';').shift();
+  }
+}
+
+export function deleteCookie(name: string) {
+  const date = new Date();
+
+  // set expiration date to yesterday
+  date.setTime(date.getTime() + (-1 * 24 * 60 * 60 * 1000));
+
+  // the cookie
+  document.cookie = `${name}=;expires=${date.toUTCString()};path=/`;
+}

5ab7bb83b0cff1398b810875895245e94fa5fe45
Ari Mendelow <ari@mendelow.me>
2020-05-25 11:52:04 -0700

    Stand-in logged-in header

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -2,6 +2,37 @@ import * as React from 'react';
 import Link from 'next/link';
 
 import UserContext from 'helpers/UserContext';
+import { logout } from 'helpers/auth';
+
+interface LoggedInHeaderProps {}
+const LoggedInHeader: React.FC<LoggedInHeaderProps> = ({}) => {
+  return(
+    <React.Fragment>
+      <button
+        className="table-cell h-full align-middle w-1/3"
+        onClick={() => logout(false)}
+      >
+        log out
+      </button>
+      <h2 className="table-cell align-middle w-1/3">
+        <img
+          className="h-10 m-auto"
+          src={require('images/logos/rj-logo-outline-nobg.svg')}
+        />
+      </h2>
+      <Link
+        href="/login"
+      >
+      <button
+        className="table-cell h-full align-middle text-right w-1/3"
+        onClick={() => logout(true)}
+      >
+        log out everywhere
+      </button>
+      </Link>
+    </React.Fragment>
+  )
+}
 
 interface NoUserHeaderProps {}
 const NoUserHeader: React.FC<NoUserHeaderProps> = ({}) => {
@@ -41,7 +72,7 @@ const Header: React.FC<HeaderProps> = ({}) => {
         className="px-5 w-screen h-14 border-b border-gray-300 table text-gray-900 flex-row justify-between content-center font-light lowercase tracking-wide text-sm fixed z-50 inset-x-0 top-0"
       >
         { isLoggedIn ?
-          <div>Logged in!</div>
+          <LoggedInHeader />
         :
           <NoUserHeader />
         }

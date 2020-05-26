0bb6698a6029c080fbb1c2a70bc0ff26c7aa622a
Ari Mendelow <ari@mendelow.me>
2020-05-26 10:20:43 -0700

    This now works - turns out the issue wasn't with short-circuiting the URL variables, but short circuiting them to empty strings, as '//' isn't a valid URL. '/#/#', however, is valid, so short circuit to that instead.

--- components/ShortRecipe.tsx
+++ components/ShortRecipe.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
+import Link from 'next/link';
 import Skeleton from 'react-loading-skeleton';
 
 import { EdgeType } from 'components/InfiniteScroll';
 import { ShortRecipeInfoType } from 'requests/recipes';
+import { ensureVarIsSet } from 'helpers/methods';
 
 interface ShortRecipeProps {
   edge: EdgeType<ShortRecipeInfoType>;
@@ -12,15 +14,20 @@ const ShortRecipe: React.FC<ShortRecipeProps> = ({
   edge
 }) => {
   const { node } = edge;
-  const { id, by, title, description, servings } = node;
+  const { id, by, title, description, servings, handle } = node || {};
+  let { username } = by || {};
   return(
     <li
       className="my-5 p-5 bg-white rounded-lg shadow-lg"
     >
-      <h3 className="font-bold text-xl">{title || <Skeleton height={30} />}</h3>
-      <div>Chef: {by.username || <Skeleton width={150} />}</div>
-      <div>{servings || <Skeleton />}</div>
-      <p>{description || <Skeleton />}</p>
+      <Link href="/[username]/[recipehandle]" as={`/${username || '#'}/${handle || '#'}`}>
+        <a>
+          <h3 className="font-bold text-xl">{title || <Skeleton height={30} />}</h3>
+          <div>Chef: {by.username || <Skeleton width={150} />}</div>
+          <div>{servings || <Skeleton />}</div>
+          <p>{description || <Skeleton />}</p>
+        </a>
+      </Link>
     </li>
   )
 }

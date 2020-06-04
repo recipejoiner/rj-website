eaff3c3668016c9032a35d54e59ce1894cdd30e8
Ari Mendelow <ari@mendelow.me>
2020-06-04 16:20:23 -0700

    Set default fetchPolicy 'no-cache'

--- requests/client.tsx
+++ requests/client.tsx
@@ -5,6 +5,19 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 const client = new ApolloClient({
   link: new HttpLink({ uri: process.env.NEXT_PUBLIC_RECIPEJOINER_API }),
   cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'no-cache',
+      errorPolicy: 'ignore',
+    },
+    query: {
+      fetchPolicy: 'no-cache',
+      errorPolicy: 'all',
+    },
+    mutate: {
+      errorPolicy: 'all',
+    },
+  },
 })
 
 /* using the above query should look as follows:

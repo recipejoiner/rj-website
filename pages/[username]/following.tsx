9dcc675bb7268d63e318e7b2f7346fd79fb163f2
Ari Mendelow <ari@mendelow.me>
2020-06-05 16:15:33 -0700

    Basics of following page

--- pages/[username]/following.tsx
+++ pages/[username]/following.tsx
@@ -1,17 +1,86 @@
 import { NextPage } from 'next'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import React from 'react'
 
 import UserContext from 'helpers/UserContext'
+import {
+  FollowRelListNodeType,
+  followConnectionNodeInit,
+  FollowRelListByUsernameVars,
+  FOLLOWING_BY_USERNAME,
+} from 'requests/users'
+import InfiniteScroll, { EdgeType } from 'components/InfiniteScroll'
+import FollowChangeBtn from 'components/FollowChangeBtn'
 
 type Props = {}
 
 const Following: NextPage<Props> = ({}) => {
   const { currentUserInfo } = React.useContext(UserContext)
+  const router = useRouter()
+  const { username } = router.query
+
+  if (username === 'undefined' || typeof username !== 'string') {
+    throw 'Username must be a valid string'
+  }
+
+  const UsersFollowRelVars: FollowRelListByUsernameVars = {
+    username: username,
+    cursor: null,
+  }
+
   return (
     <React.Fragment>
       {/* <Head></Head> */}
-      <div>Following list goes here</div>
+      <InfiniteScroll
+        QUERY={FOLLOWING_BY_USERNAME}
+        hasJustConnection={false}
+        nodeInit={followConnectionNodeInit}
+        QueryVars={UsersFollowRelVars}
+      >
+        {(edges: Array<EdgeType<FollowRelListNodeType>>) => {
+          const [
+            followingStatusRecord,
+            setFollowingStatusRecord,
+          ] = React.useState<Record<string, boolean | null>>({})
+          const setFollowingStatus = (
+            username: string,
+            status: boolean | null
+          ) => {
+            const newFollowingStatusRecord = { ...followingStatusRecord }
+            newFollowingStatusRecord[username] = status
+            setFollowingStatusRecord(newFollowingStatusRecord)
+          }
+          return (
+            <ul>
+              {edges.map((edge) => {
+                // make sure that the edge is defined (and not the initializer), and that the followStatus of this username hasn't yet been initialized
+                // else, it'll keep trying to update that status and throw a 'too many re-renders' error
+                if (
+                  edge.cursor !== '' &&
+                  followingStatusRecord[edge.node.username] === undefined
+                ) {
+                  setFollowingStatus(edge.node.username, edge.node.areFollowing)
+                }
+                return (
+                  <li key={edge.cursor}>
+                    <h3>{edge.node.username}</h3>
+                    <FollowChangeBtn
+                      followingStatus={
+                        followingStatusRecord[edge.node.username]
+                      }
+                      setFollowingStatus={(status: boolean) => {
+                        setFollowingStatus(edge.node.username, status)
+                      }}
+                      username={edge.node.username}
+                    />
+                  </li>
+                )
+              })}
+            </ul>
+          )
+        }}
+      </InfiniteScroll>
     </React.Fragment>
   )
 }

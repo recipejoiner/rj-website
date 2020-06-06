1144947e5dd89435fe82cc59f47e7d6a9b486c70
Ari Mendelow <ari@mendelow.me>
2020-06-06 15:49:02 -0700

    Move AppModal into own component

---
+++ components/AppModal.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react'
+import Modal from 'react-modal'
+Modal.setAppElement('#__next') // Make sure to bind modal to your appElement (http://reactcommunity.org/react-modal/accessibility/)
+
+interface AppModalProps {
+  modalOpen: boolean
+  setModalOpen: (state: boolean) => void
+}
+
+const AppModal: React.FC<AppModalProps> = ({ setModalOpen, children }) => {
+  return (
+    <Modal
+      isOpen={true}
+      className="w-80 h-128 m-auto bg-white overflow-scroll mt-48 p-2 rounded border shadow-xl"
+      style={{
+        overlay: { backgroundColor: '#000000bf' },
+      }}
+      onRequestClose={() => {
+        setModalOpen(false)
+      }}
+      contentLabel="User modal"
+    >
+      {children}
+    </Modal>
+  )
+}
+
+export default AppModal

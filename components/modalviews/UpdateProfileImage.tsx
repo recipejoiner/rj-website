038007cd5be94ec2722e295458811c40b94a2730
Ari Mendelow <ari@mendelow.me>
2020-06-11 11:53:06 -0700

    Working form, but still need to reflect the update on client

--- components/modalviews/UpdateProfileImage.tsx
+++ components/modalviews/UpdateProfileImage.tsx
@@ -13,28 +13,33 @@ type UpdateProfileImageProps = {}
 
 const UpdateProfileImage: React.FC<UpdateProfileImageProps> = ({}) => {
   const linkStyle =
-    'w-full px-8 py-4 block font-semibold hover:text-gray-700 text-center text-sm tracking-widest border-b border-gray-300'
+    'w-full px-8 py-4 block font-semibold hover:text-gray-700 focus:text-gray-900 text-center text-sm tracking-widest border-b border-gray-300 focus:outline-none focus:shadow-outline'
 
   const [imageErrs, setImageErrs] = React.useState<Array<gqlError>>([])
 
-  const { register, handleSubmit, watch, errors } = useForm<
-    SetProfileImageVarsType
-  >()
+  const { register, handleSubmit, watch, errors } = useForm<{
+    files: FileList
+  }>()
 
-  const onSubmit = handleSubmit((variables: SetProfileImageVarsType) => {
-    const token = process.env.NEXT_PUBLIC_RJ_API_TOKEN || ''
+  console.log('watch', watch('profileImage'))
+
+  const onSubmit = handleSubmit(({ files }) => {
+    console.log('files', files)
+    const variables: SetProfileImageVarsType = {
+      profileImage: files[0],
+    }
     client
       .mutate({
         mutation: SET_PROFILE_IMAGE,
         variables: variables,
         context: {
-          // example of setting the headers with context per operation
           headers: {
             authorization: `Bearer ${getToken()}`,
           },
         },
       })
       .then((res) => {
+        console.log('res', res)
         const { data }: { data?: SetProfileImageReturnType } = res || {}
         if (!!data) {
           console.log('data:', data)
@@ -44,14 +49,27 @@ const UpdateProfileImage: React.FC<UpdateProfileImageProps> = ({}) => {
       })
       .catch((err) => {
         setImageErrs(err.graphQLErrors)
-        console.log(imageErrs)
+        console.log('imageErrs', imageErrs)
       })
   })
 
   return (
     <React.Fragment>
       <form onSubmit={onSubmit}>
-        <input name="profileImage" type="file" accept="image/*" />
+        <label
+          className={`${linkStyle} text-blue-600 focus:text-red-700 cursor-pointer`}
+        >
+          Upload Photo
+          <input
+            className="hidden"
+            name="files"
+            type="file"
+            multiple={false}
+            accept="image/*"
+            onChange={onSubmit}
+            ref={register}
+          />
+        </label>
       </form>
     </React.Fragment>
   )

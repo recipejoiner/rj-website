3534ea2e7105e3253b83287bcf35f775f2c73ae8
Ari Mendelow <ari@mendelow.me>
2020-06-30 23:13:31 -0400

    Add file for new comment form

---
+++ components/comments/NewCommentForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import { GraphQLError } from 'graphql'
+
+interface NewCommentFormProps {}
+
+const NewCommentForm: React.FC<NewCommentFormProps> = ({}) => {
+  return <div>New Comment Form</div>
+}
+
+export default NewCommentForm

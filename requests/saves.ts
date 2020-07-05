858a22edfa0bff56b4acdaa1595815f97b80cf4a
Ari Mendelow <ari@mendelow.me>
2020-07-05 01:19:59 -0400

    Moved save mutation into its own file. Also, remove unused import

---
+++ requests/saves.ts
@@ -0,0 +1,27 @@
+import gql from 'graphql-tag'
+
+export interface SetSavedResultType {
+  result: {
+    saved: boolean
+  }
+}
+export interface SetSavedVarsType {
+  saveableType: 'Recipe'
+  saveableId: string
+  savedState: boolean
+}
+export const SET_OBJECT_SAVED = gql`
+  mutation setObjectSaved(
+    $savableType: String!
+    $saveableId: ID!
+    $savedState: Boolean!
+  ) {
+    result: setObjectSaved(
+      saveableType: $saveableType
+      saveableId: $saveableId
+      savedState: $savedState
+    ) {
+      saved
+    }
+  }
+`

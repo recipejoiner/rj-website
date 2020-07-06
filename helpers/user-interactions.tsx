867bb5812c1948e2c9665d8357cef89631b7c94d
Ari Mendelow <ari@mendelow.me>
2020-07-06 18:17:44 -0400

    Add setSaved handler for saving recipes

--- helpers/user-interactions.tsx
+++ helpers/user-interactions.tsx
@@ -9,6 +9,11 @@ import {
   SetReactionVarsType,
   SetReactionResultType,
 } from 'requests/reactions'
+import {
+  SET_OBJECT_SAVED,
+  SetSavedResultType,
+  SetSavedVarsType,
+} from 'requests/saves'
 import { CurrentUserLoginCheckType } from 'requests/auth'
 
 export const setReaction = (
@@ -37,6 +42,32 @@ export const setReaction = (
     })
 }
 
+export const setSaved = (
+  savedParams: SetSavedVarsType,
+  callback: (result: SetSavedResultType | readonly GraphQLError[]) => any
+): void => {
+  const mutationResult = client
+    .mutate({
+      mutation: SET_OBJECT_SAVED,
+      variables: savedParams,
+      context: {
+        headers: {
+          authorization: `Bearer ${getToken()}`,
+        },
+      },
+    })
+    .then((res) => {
+      console.log('res', res)
+      if (res.errors) {
+        callback(res.errors)
+      }
+      const { data }: { data?: SetSavedResultType } = res || {}
+      if (data?.result) {
+        callback(data)
+      }
+    })
+}
+
 export const setYumHandler = (
   currentUserInfo: CurrentUserLoginCheckType | undefined,
   recipeId: string,
@@ -53,6 +84,32 @@ export const setYumHandler = (
       (result) => {
         if ('result' in result) {
           setRecipeReaction(result.result.reaction)
+        } else {
+          console.log('error', result)
+        }
+      }
+    )
+  } else {
+    console.log('You need to log in or sign up!')
+  }
+}
+
+export const setRecipeSavedHandler = (
+  recipeId: string,
+  currentSavedState: boolean | null,
+  setSavedState: (value: React.SetStateAction<boolean | null>) => void
+) => {
+  // will be null if not logged in
+  if (currentSavedState != null) {
+    setSaved(
+      {
+        saveableId: recipeId,
+        saveableType: 'Recipe',
+        savedState: !currentSavedState,
+      },
+      (result) => {
+        if ('result' in result) {
+          setSavedState(result.result.saved)
         } else {
           console.log('error', result)
         }

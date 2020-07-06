ca31c9f39047b41410647a9816431d9167bdd4a7
Ari Mendelow <ari@mendelow.me>
2020-07-06 10:52:51 -0400

    Fix type name and add 'setReaction' handler

--- requests/reactions.ts
+++ requests/reactions.ts
@@ -1,4 +1,8 @@
 import gql from 'graphql-tag'
+import { GraphQLError } from 'graphql'
+
+import client from 'requests/client'
+import { getToken } from 'helpers/auth'
 
 /*
   expand this as needed, per the backend
@@ -11,7 +15,7 @@ export interface SetReactionResultType {
     reaction: ReactionType
   }
 }
-export interface SetReactionResultVarsType {
+export interface SetReactionVarsType {
   reactableType: 'Comment' | 'Recipe'
   reactableId: string
   reactionType?: ReactionType
@@ -31,3 +35,28 @@ export const SET_OBJECT_REACTION = gql`
     }
   }
 `
+
+export const setReaction = (
+  reactionParams: SetReactionVarsType,
+  callback: (result: SetReactionResultType | readonly GraphQLError[]) => any
+): void => {
+  const mutationResult = client
+    .mutate({
+      mutation: SET_OBJECT_REACTION,
+      variables: reactionParams,
+      context: {
+        headers: {
+          authorization: `Bearer ${getToken()}`,
+        },
+      },
+    })
+    .then((res) => {
+      if (res.errors) {
+        callback(res.errors)
+      }
+      const { data }: { data?: SetReactionResultType } = res || {}
+      if (data?.result) {
+        callback(data)
+      }
+    })
+}
